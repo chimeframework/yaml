e09b3a1ff91ec9157ec634f5f8402963c9b69b4b
Ashok Gelal <ashokgelal@gmail.com>
2011-11-20 01:23:01 -0700

    Load Yaml

--- src/yaml.go
+++ src/yaml.go
@@ -1,33 +1,34 @@
 package yaml
 
 import (
-    "bytes"
-    "fmt"
-    "launchpad.net/goyaml"
-    "os"
+	"bytes"
+	"fmt"
+	"launchpad.net/goyaml"
+	"os"
 )
 
-func Parse(path string) map[interface{}]interface{}{
-   if data, err := ReadFileAsBytes(path); err != nil{
-       panic(fmt.Sprintf("Error while reading YAML file %v", path))
-   }
+func Parse(path string) map[interface{}]interface{} {
+	data, err := ReadFileAsBytes(path)
+	if err != nil {
+		panic(fmt.Sprintf("Error while reading YAML file %v", path))
+	}
 
-   output := make[map[interface{}]interface{}]
+	output := make(map[interface{}]interface{})
 
-   if err := goyaml.Unmarshal(data, &output); err != nil{
-       panic(err)
-   }
+	if err := goyaml.Unmarshal(data, &output); err != nil {
+		panic(err)
+	}
 
-   return output
+	return output
 }
 
 func ReadFileAsBytes(filename string) (contents []byte, err error) {
-    file, e := os.OpenFile(filename, os.O_RDONLY, 0644)
-    if e != nil {
-        return nil, e
-    }
+	file, e := os.OpenFile(filename, os.O_RDONLY, 0644)
+	if e != nil {
+		return nil, e
+	}
 
-    var buf bytes.Buffer
-    buf.ReadFrom(file)
-    return buf.Bytes(), nil
+	var buf bytes.Buffer
+	buf.ReadFrom(file)
+	return buf.Bytes(), nil
 }
